7762b185a70139f5ca14381db73d25be2af9587c
yuqianw2002 <wang.yuqian2@northeastern.edu>
2025-08-27 16:06:24 -0400

    update py

---
+++ hello.py
@@ -0,0 +1,9 @@
+def say_hello(name: str) -> str:
+    """Return a greeting message to students in the IDS class."""
+    return f"Hello, {name}, welcome to Data Engineering Systems (IDS 706)!"
+
+
+def add(a: int, b: int) -> int:
+    """Return the sum of two numbers."""
+    return a + b
+    
